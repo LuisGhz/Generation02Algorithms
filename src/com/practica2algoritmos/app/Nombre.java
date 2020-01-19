dda9d9b9ea3bf4aa5c2321ab14c3834b22f87488
Luis Gutiérrez <carver.anarquia@gmail.com>
2020-01-19 14:17:15 -0600

    Second project in java

---
+++ src/com/practica2algoritmos/app/Nombre.java
@@ -0,0 +1,30 @@
+package com.practica2algoritmos.app;
+
+public class Nombre {
+
+	public static void main(String[] args) {
+		
+		//To Do:
+		//Declaración de variables
+		String nombreEstudiante = "Ari";
+		int telefonoEstudiante = 3215646;
+		boolean isCasada = true;
+		char sexoEstudiante = 'F';
+		double estaturaEstudiante = 1.50;
+		float calificacionEstudiante = 3.2f;
+		byte edadEstudiante = 15;
+		short vecesSaleShoping = 200;
+		
+		//Mostrar Mensajes
+		System.out.println("Dame tu nombre: " + nombreEstudiante);
+		System.out.println("Este es un entero: " + telefonoEstudiante);
+		System.out.println("Este es un boleano: " + isCasada);
+		System.out.println("Este es un caracter: " + sexoEstudiante);
+		System.out.println("Este es un double: " + estaturaEstudiante);
+		System.out.println("Este es un float: " + calificacionEstudiante);
+		System.out.println("Este es un byte: " + edadEstudiante);
+		System.out.println("Este es un short: " + vecesSaleShoping);
+		
+	}
+
+}
